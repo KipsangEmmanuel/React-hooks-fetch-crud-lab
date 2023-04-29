c39dbcfc2788bfcfa66d57c43ed1c27e70649feb
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-29 16:06:11 -0700

    add delete function

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -12,6 +12,17 @@ function QuestionList() {
     .then((res) => res.json())
     .then(data => setQuestions(data))
   }, []);// The empty dependency array [] passed to useEffect ensures that the effect runs only once when the component mounts.
+
+  function handleDeleteQuestion(id) {
+    fetch(`/http://localhost:3002/questions/${id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    })
+    .then(() => {
+      setQuestions(questions.filter((question) => question.id !== id));
+    });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
@@ -19,7 +30,10 @@ function QuestionList() {
         {/* display QuestionItem components here after fetching */}
         {/* Map over the questions state variable to render a QuestionItem component for each question: */}
         {questions.map(questions => (
-          <QuestionItem key={questions.id} question={questions} />
+          <QuestionItem 
+          key={questions.id}
+          question={questions}
+           onDelete={handleDeleteQuestion} />
         ))}
       </ul>
     </section>
